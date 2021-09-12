b5eecfc9957430e2daec4d8d7e06ea6083adad64
CAMAPY <ironmancaptain86@gmail.com>
2021-09-12 14:23:41 +0400

    init commit

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -51,27 +51,27 @@ class moderation(commands.Cog):
         await ctx.send(f'Nickname was changed for `@{member.name}` ')
 
     @commands.Cog.listener()
-    async def on_message(message): 
+    async def on_message(self, message): 
         if "what the fuck did you just fucking say about me, you little bitch?" in message.content.lower():
             await message.channel.send(f"stop. posting. this. it isn't funny, {message.author.mention}")
             await message.delete()
     @commands.Cog.listener()
-    async def on_message(message):  
+    async def on_message(self, message):  
         if "fap" in message.content.lower():
             if message.author.id == 275609153274380289:
                 await message.channel.send("Bruh, koshy don't fap again")
     @commands.Cog.listener()
-    async def on_message(message):  
+    async def on_message(self, message):  
         if "ironic" in message.content.lower():
             if message.author.id == 578865305632243712:
                 await message.channel.send("Learn to use `ironic` properly u dumbfuck")
     @commands.Cog.listener()
-    async def on_message(message): 
+    async def on_message(self, message): 
         possible_responses = [ 'hello', 'suppers', 'yo wassup', 'stfu dumbass', 'what do u want this time?', 'ok now what?', 'hello daddy :drooling_face:' ] 
         if "cambot" in message.content.lower():
             await message.channel.send(f"{random.choice(possible_responses)} {message.author.mention}")
     @commands.Cog.listener()
-    async def on_message(message):  
+    async def on_message(self, message):  
         if "^" in message:
             if message.author.id == 398429963990335489:
                 await message.channel.send("your mom's exponential weight gain be like")
