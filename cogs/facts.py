0cf76c9c28a55b8fd2021a5f9a10e67bd1af289b
CAMAPY <ironmancaptain86@gmail.com>
2021-09-14 18:21:28 +0400

    init commit

--- cogs/facts.py
+++ cogs/facts.py
@@ -24,8 +24,9 @@ class facts(commands.Cog):
                 await session.close()
     @commands.command()
     async def fact(self,ctx):
+        colors = [0xFFE4E1, 0x00FF7F, 0xD8BFD8, 0xDC143C, 0xFF4500, 0xDEB887, 0xADFF2F, 0x800000, 0x4682B4, 0x006400, 0x808080, 0xA0522D, 0xF08080, 0xC71585, 0xFFB6C1, 0x00CED1]
         fact = random.choice(open("facts.txt","r").readlines())
-        fax = discord.Embed(title="Random fact", description=f'{fact}')
+        fax = discord.Embed(title="Random fact", description=f'{fact}', color = random.choice(colors))
         await ctx.send(embed = fax)
 
 def setup(client):
