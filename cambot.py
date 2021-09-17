e97e6dad0b6863448f9c6b86af1015cf144452c6
CAMAPY <ironmancaptain86@gmail.com>
2021-09-17 22:25:04 +0400

    init commit

--- cambot.py
+++ cambot.py
@@ -44,7 +44,7 @@ async def calculate(ctx,*,expression):
 async def rename(name):
     await client.user.edit(username=name)
 
-@client.command(hidden =True)
+@client.command(aliases = ["s"],hidden =True)
 @commands.is_owner()
 async def status(ctx, *, status=''):            #im the 
     # if status == None:
@@ -52,7 +52,7 @@ async def status(ctx, *, status=''):            #im the
     await client.change_presence(status=discord.Status.online, activity=discord.Game(status))
     await ctx.send(f'launching {status}.')
 
-@client.command(hidden = True)
+@client.command(aliases = ["quit", "q"],hidden = True)
 @commands.is_owner()
 async def close(ctx):
     await ctx.send(f"Ok bye! :wave:")
