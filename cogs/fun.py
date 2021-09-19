7f37702b3aefa80af25b6249e3bfc7ec920ac60b
CAMAPY <ironmancaptain86@gmail.com>
2021-09-19 11:31:57 +0400

    init commit

--- cogs/fun.py
+++ cogs/fun.py
@@ -35,7 +35,7 @@ class Fun(commands.Cog):
 
     @commands.command(aliases = ["ng"])
     async def numbergame(self,ctx):
-        num = random.randint(1,146) 
+        num = random.randint(1,134) 
         if num in range(1,45):
             await ctx.send('https://media.discordapp.net/attachments/862667294428889129/886633593957417000/unknown.png?width=1253&height=701')
         elif num in range(45, 100):
