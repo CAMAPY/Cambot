1777b3844996dc46e1e4ddf0f250ac0b75a17f7d
CAMAPY <ironmancaptain86@gmail.com>
2021-09-26 17:11:55 +0400

    init commit

--- cogs/HappyNewDay.py
+++ cogs/HappyNewDay.py
@@ -1,12 +1,9 @@
-
+import schedule
 import time
 import discord
-import sys
 from discord.ext import commands
 from pretty_help import PrettyHelp,DefaultMenu
-import random
 from math import *
-import os
 from discord.ext.commands.converter import MemberConverter
 from discord.ext import tasks
 
@@ -21,7 +18,7 @@ class wish(commands.Cog):
         channel = client.get_channel(874866927938449471)
         await channel.send("Happy New Day!")
     # Run job every day at specific HH:MM and next HH:MM:SS
-    schedule.every().day.at("22:30").do(job)
+    schedule.every().day.at("17:15").do(job)
     while True:
         schedule.run_pending()
         time.sleep(1)
