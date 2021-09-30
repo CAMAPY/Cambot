83741ba516aaaec9d8e39422785afe241e8140af
CAMAPY <ironmancaptain86@gmail.com>
2021-09-30 10:45:20 +0400

    init commit

---
+++ cogs/test.py
@@ -0,0 +1,28 @@
+import discord
+import sys
+from discord.ext import commands
+from pretty_help import PrettyHelp,DefaultMenu
+import random
+from math import *
+import os
+from discord.ext.commands.converter import MemberConverter
+ 
+
+class Test(commands.Cog):
+
+    def __init__(self,client): 
+        self.client = client
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f'Test has been turned on {self.client.user}')
+
+
+
+    @commands.command(aliases=["t"],brief="Literally just replies with whats in your message after the command")
+    async def test(self,ctx,*, arg):
+        await ctx.message.delete()
+        await ctx.send(arg)
+        channel = self.client.get_channel(872371310704066633)
+        await channel.send(f"{arg}, requested by {ctx.author.name}")
+def setup(client):
+    client.add_cog(Test(client))
