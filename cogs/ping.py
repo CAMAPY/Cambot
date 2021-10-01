2c4a2e65200b4e65071b32e8498bab9e20f6d726
CAMAPY <ironmancaptain86@gmail.com>
2021-10-01 17:19:37 +0400

    init commit

--- cogs/ping.py
+++ cogs/ping.py
@@ -15,13 +15,9 @@ class ping(commands.Cog):
     async def on_ready(self):
         print(f'`ping` reply has been turned on {self.client.user}')
     
-    def check(m):
-        if m.reference is not None and not m.is_system :
-            return True
-        return False
 
     @commands.Cog.listener()
-    async def on_ping(message, check = check):
+    async def on_ping(message):
         colors = [0xFFE4E1, 0x00FF7F, 0xD8BFD8, 0xDC143C, 0xFF4500, 0xDEB887, 0xADFF2F, 0x800000, 0x4682B4, 0x006400, 0x808080, 0xA0522D, 0xF08080, 0xC71585, 0xFFB6C1, 0x00CED1]
         reply = discord.Embed(title="Hello :wave:", description=f"I'm a bot sent to learn your ways. My prefix is `;`. Use `;help` if you wanna know more Get my only-fans here: [CambotHelp.com](https://www.youtube.com/watch?v=dQw4w9WgXcQ)", color = random.choice(colors))
         await message.channel.send(embed = reply)
