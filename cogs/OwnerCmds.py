6546026116758a2fafb42e6c065d3160168825b3
CAMAPY <ironmancaptain86@gmail.com>
2021-10-01 06:57:10 +0400

    init commit

--- cogs/OwnerCmds.py
+++ cogs/OwnerCmds.py
@@ -59,7 +59,7 @@ class OwnerCommands(commands.Cog):
 
     @commands.command(hidden = True)
     @commands.is_owner()
-    async def message(user: discord.Member,*,arg):
-        await user.send(f"{arg}")
+    async def message(user: discord.Member,*,message = "No message entered"):
+        await user.send(f"{message}")
 def setup(client):
     client.add_cog(OwnerCommands(client))
