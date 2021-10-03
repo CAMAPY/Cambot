cc516e798979fe53890fc0338868d9e6d9bb9cbf
CAMAPY <ironmancaptain86@gmail.com>
2021-10-03 13:45:28 +0400

    init commit

--- cogs/techsupport.py
+++ cogs/techsupport.py
@@ -24,8 +24,8 @@ class TechSupport(commands.Cog, description = "Tech-support"):
         await ctx.send("Please choose your problem from the following options: (enter the number)")
         if device == "Automated Dildo":
             await ctx.send("""1)Dildo not extending
-            2)Dildo not as sensual as before
-            3)I think my dildo is alive""")      
+2)Dildo not as sensual as before
+3)I think my dildo is alive""")      
         def check(message):
             return message.content=="1" or message.content.lower() == '2' or message.content.lower() == '3' and message.author == ctx.author  
         choice = (await self.client.wait_for('message', check=check)).content 
