f70b536bc66bc65de34aa3d78295f0556fe73be7
CAMAPY <ironmancaptain86@gmail.com>
2021-10-09 14:16:49 +0400

    init commit

--- cogs/techsupport.py
+++ cogs/techsupport.py
@@ -24,65 +24,88 @@ class TechSupport(commands.Cog, description = "Tech-support"):
         await ctx.send(f"Good day Sir/Madam, Thank you for calling tech-support regarding your {device}")
         await ctx.send("Please choose your problem from the following options: (enter the number)")
         def check(message):
-            return (message.content == "1" or message.content == '2' or message.content == '3') and message.author == ctx.author 
+            return (message.content.lower() == "y" or message.content.lower() == 'n') and message.author == ctx.author 
         async def loading():
-            await ctx.send("Connecting... <a:loading:894873095737856010>")
             await ctx.send(f"Please wait patiently while we connect you to our specialists")
             await asyncio.sleep(5)
             await ctx.send(f"Our Helpers are busy at the moment, We will get to you in a minute")
             await asyncio.sleep(5)
             await ctx.send(f"Please consider checking the manual or our support forums for a solution before calling us")
+        async def reconnect():
+            await ctx.send(f"""Connection has been lost
+            Would you like to try to reconnect? (y/n)""")
+        def check1(message):
+            return (message.content == "1" or message.content == '2' or message.content == '3') and message.author == ctx.author 
         if device == "Automated Dildo":
             await ctx.send("""`1) Dildo not extending
 2) Dildo not as sensual as before
 3) I think my dildo is alive`""")      
             choice = (await self.client.wait_for('message', check=check)).content 
+            await ctx.send("Connecting... <a:loading:894873095737856010>")
             await loading()
-            await loading()
-            if choice == "1":
-                solve = discord.Embed(title = "Solution", description = "Stroke dildo soothingly")
-                await ctx.send(embed = solve)
-            elif choice == "2":
-                solve = discord.Embed(title = "Solution", description = "Apply lube or check for dents in dildo")
-                await ctx.send(embed = solve)
-            elif choice == "3":
-                solve = discord.Embed(title = "Solution", description = "WHATEVER YOU DO, DO NOT FEED IT")
-                await ctx.send(embed = solve)
-            await asyncio.sleep(5)
-            await ctx.send("Call disconnected")
+            await reconnect()
+            option = (await self.client.wait_for('message', check=check1)).content 
+            if option.lower() == "y":
+                await loading()
+                if choice == "1":
+                    solve = discord.Embed(title = "Solution", description = "Stroke dildo soothingly")
+                    await ctx.send(embed = solve)
+                elif choice == "2":
+                    solve = discord.Embed(title = "Solution", description = "Apply lube or check for dents in dildo")
+                    await ctx.send(embed = solve)
+                elif choice == "3":
+                    solve = discord.Embed(title = "Solution", description = "WHATEVER YOU DO, DO NOT FEED IT")
+                    await ctx.send(embed = solve)
+                await asyncio.sleep(2)
+                await ctx.send("Thank you for reaching out to us, call disconnected")
+            elif option.lower() == "n":
+                await ctx.send("Call disconnected")
         elif device == "Automatic pet feeder":
             await ctx.send("""`1) Pet not eating`
 2) Food smells weird`""")      
             choice = (await self.client.wait_for('message', check=check)).content 
+            await ctx.send("Connecting... <a:loading:894873095737856010>")
             await loading()
-            await loading()
-            if choice == "1":
-                solve = discord.Embed(title = "Solution", description = "Forcefeed the pet until it begs for mercy")
-                await ctx.send(embed = solve)
-            elif choice == "2":
-                solve = discord.Embed(title = "Solution", description = "Food is produced by our trustworthy partner PetFudz. Do not worry if pet starts vomitting, throw pet away")
-                await ctx.send(embed = solve)
-            elif choice == "3":
-                await ctx.send("That's not a real problem you nitwit")
-            await ctx.send("Call disconnected.")
+            await reconnect()
+            option = (await self.client.wait_for('message', check=check1)).content 
+            if option.lower() == "y":
+                await loading()
+                if choice == "1":
+                    solve = discord.Embed(title = "Solution", description = "Forcefeed the pet until it begs for mercy")
+                    await ctx.send(embed = solve)
+                elif choice == "2":
+                    solve = discord.Embed(title = "Solution", description = "Food is produced by our trustworthy partner PetFudz. Do not worry if pet starts vomitting, throw pet away")
+                    await ctx.send(embed = solve)
+                elif choice == "3":
+                    await ctx.send("That's not a real problem you nitwit")
+                await asyncio.sleep(2)
+                await ctx.send("Thank you for reaching out to us, call disconnected")
+            elif option.lower() == "n":
+                await ctx.send("Call disconnected")
 
         elif device == "PlayStation 2":
             await ctx.send("""`1) PS2 not starting up
 2) PS2 having display error
 3) PS2 is making a ticking noise`""")    
             choice = (await self.client.wait_for('message', check=check)).content 
+            await ctx.send("Connecting... <a:loading:894873095737856010>")
             await loading()
-            await loading()
-            if choice == "1":
-                solve = discord.Embed(title = "Solution", description = "Plug PS2 into power socket and press power button")
-                await ctx.send(embed = solve)
-            elif choice == "2":
-                solve = discord.Embed(title = "Solution", description = "Make sure PS2 not connected to microwave display")
-                await ctx.send(embed = solve)
-            elif choice == "3":
-                solve = discord.Embed(title = "Solution", description = "Quick, take PS2 and enter World Trade Centre")
-                await ctx.send(embed = solve)
-            await ctx.send("Call disconnected.")
+            await reconnect()
+            option = (await self.client.wait_for('message', check=check1)).content 
+            if option.lower() == "y":
+                if choice == "1":
+                    solve = discord.Embed(title = "Solution", description = "Plug PS2 into power socket and press power button")
+                    await ctx.send(embed = solve)
+                elif choice == "2":
+                    solve = discord.Embed(title = "Solution", description = "Make sure PS2 not connected to microwave display")
+                    await ctx.send(embed = solve)
+                elif choice == "3":
+                    solve = discord.Embed(title = "Solution", description = "Quick, take PS2 and enter World Trade Centre")
+                    await ctx.send(embed = solve)
+                await asyncio.sleep(2)
+                await ctx.send("Thank you for reaching out to us, call disconnected")
+            elif option.lower() == "n":
+                await ctx.send("Call disconnected")
 
     
 
