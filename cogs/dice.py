4e41e8c08139db9512030be6960f167fa018dbd0
CAMAPY <ironmancaptain86@gmail.com>
2021-10-20 13:43:11 +0400

    init commit

--- cogs/dice.py
+++ cogs/dice.py
@@ -24,7 +24,7 @@ class dice(commands.Cog, description = "Get your life-choices done with these di
         await ctx.channel.send(f"{numbers}; {number}, {ctx.author.name}")
     @commands.command()
     async def choose(self, ctx, choices):
-        await ctx.send(choices.partition(' '))
-        await ctx.send(f"I choose `{random.choice(choices.partition(' '))}`, By the power vested in me by MakeDumbassDecisions.com")
+        await ctx.send(choices.split(' '))
+        await ctx.send(f"I choose `{random.choice(choices.split(' '))}`, By the power vested in me by MakeDumbassDecisions.com")
 def setup(client):
     client.add_cog(dice(client))
