7bafa23101b32110f04c15665a387451d2474472
CAMAPY <ironmancaptain86@gmail.com>
2022-03-21 23:04:28 +0530

    bro

--- cogs/alive-a-bot.py
+++ cogs/alive-a-bot.py
@@ -40,7 +40,9 @@ class aliveabot(commands.Cog, description = "Responses that make the bot seem al
             channel = self.client.get_channel(873983296269348884)
             await channel.send(f"{message.content},  by {message.author.name}")
         elif "moo" in message.content.lower():
-            list = ["he's gon say he loves u but he cappin', honey", "he may love you but im fucking ur mom", ""]
+            list = ["he's gon say he loves u but he cappin', honey", "he may love you but im fucking ur mom"]
+            n = random.randrange(1, 3)
+            await message.reply(f"{list[n]}", mention_author = False)
         elif "i'm" in message.content.lower():
             if "dad" in message.content.lower():
                 if message.author.id == 756391739056586773:
