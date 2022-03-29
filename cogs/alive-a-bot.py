910b4f3ed02856afe7ee7757c3ff09cef9a64a76
CAMAPY <ironmancaptain86@gmail.com>
2022-03-29 18:01:52 +0530

    tst2

--- cogs/alive-a-bot.py
+++ cogs/alive-a-bot.py
@@ -24,7 +24,7 @@ class aliveabot(commands.Cog, description = "Responses that make the bot seem al
         elif message.content.startswith(";t"):
             return
         elif "test" in message.content.lower():
-            await message.send("bruh")
+            await message.reply("bruh")
         elif "^" in message.content and message.author.id == 398429963990335489:
                 await message.reply("your mom's exponential weight gain be like", mention_author = False)
         elif "im" in message.content.lower() and "hi" in message.content.lower():
